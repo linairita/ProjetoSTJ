47beb97d8bec22cc102171574f993d729f0b6211
linairita <beatriz.irita@scitecjr.com>
2025-09-13 18:30:29 -0300

    Atualização de arquivos

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-import express from "express"
+import express from "express";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname);
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + "/src/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/src/pages/index.html");
+});
+
+app.get("sobre", (req, res) => {
+    res.sendFile(__dirname + "/src/pages/sobre.html");
+});
+
+app.get("contato", (req, res) => {
+    res.sendFile(__dirname + "/src/pages/contato.html");
+});
+
+app.get("produtos", (req, res) => {
+    res.sendFile(__dirname + "/src/pages/produtos.html");
+});
+
+app.listen(port, () => {
+    console.log(`>App listening at http:localhost:${port}`);
+});
