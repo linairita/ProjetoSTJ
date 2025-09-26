1bfffe222a7f64961f4f216d00506ddefe83f17b
beatrizpsantos22 <bpsantos022@gmail.com>
2025-09-26 17:49:21 -0300

    Página Inicial

--- index.js
+++ index.js
@@ -1,52 +1,32 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public')); 
-
-app.get('/', (req, res) => {
-});
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
-
-
-
-
-
-import express from "express";
-
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
+import express from 'express';
+import path, { join } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-console.log(__dirname);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(express.static(__dirname + "/src/public"));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/src/pages/index.html");
+app.use(express.static(join(__dirname, 'src', 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'src','pages','index.html'));
 });
 
-app.get("sobre", (req, res) => {
-    res.sendFile(__dirname + "/src/pages/sobre.html");
+app.get('/sobre', (req, res) => {
+    res.sendFile(join(__dirname, 'src','pages','sobre.html'));
 });
 
-app.get("contato", (req, res) => {
-    res.sendFile(__dirname + "/src/pages/contato.html");
+app.get('/contato', (req, res) => {
+    res.sendFile(join(__dirname, 'src' ,'pages','contato.html'));
 });
 
-app.get("produtos", (req, res) => {
-    res.sendFile(__dirname + "/src/pages/produtos.html");
+app.get('/produtos', (req, res) => {
+    res.sendFile(join(__dirname, 'src','pages','produtos.html'));
 });
 
-// Inicia o servidor e o faz escutar na porta definida
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
